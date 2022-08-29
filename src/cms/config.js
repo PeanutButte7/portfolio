3d5fbea4ef1db7c8fb30b7ad7e7e841c439c1ea7
PeanutButte7 <adam.barta007@gmail.com>
2022-08-29 14:18:35 +0200

    CMS structure

---
+++ src/cms/config.js
@@ -0,0 +1,46 @@
+export default {
+    cms_manual_init: true,
+    backend: {
+        name: 'github',
+        repo: 'PeanutButte7/portfolio',
+        branch: 'main',
+    },
+    media_folder: 'public/img',
+    public_folder: 'img',
+    collections: [
+        {
+            name: 'project',
+            label: 'Project',
+            folder: 'project',
+            create: true,
+            fields: [
+                {
+                    name: 'title',
+                    label: 'Title',
+                    widget: 'string',
+                },
+                {
+                    name: 'description',
+                    label: 'Description',
+                    widget: 'string',
+                },
+                {
+                    name: 'link',
+                    label: 'Link',
+                    widget: 'string',
+                },
+                {
+                    name: 'type',
+                    label: 'Type',
+                    widget: 'select',
+                    options: ['web', 'design', 'game'],
+                },
+                {
+                    name: 'image',
+                    label: 'Image',
+                    widget: 'image',
+                },
+            ],
+        },
+    ],
+};
---
+++ src/pages/_app.tsx
@@ -0,0 +1,12 @@
+import { Layout } from 'components/Layout';
+import type { AppProps } from 'next/app';
+
+function MyApp({ Component }: AppProps) {
+  return (
+      <Layout>
+        <Component />
+      </Layout>
+  );
+}
+
+export default MyApp;
