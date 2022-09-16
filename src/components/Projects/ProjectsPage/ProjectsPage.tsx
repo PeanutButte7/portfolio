fe21797aa4305dc231fbaa55e822059a6f1a27c0
PeanutButte7 <adam.barta007@gmail.com>
2022-09-16 16:31:34 +0200

    Minor changes, removed console.log

--- src/components/Projects/ProjectsPage/ProjectsPage.tsx
+++ src/components/Projects/ProjectsPage/ProjectsPage.tsx
@@ -21,14 +21,13 @@ interface ProjectsPageProps {
     projects: Project[];
 }
 
-export function ProjectsPage({ projects }: ProjectsPageProps) {
+export function ProjectsPage({projects}: ProjectsPageProps) {
     const filters = [
-        { type: Filter.Web, label: 'Web apps' },
-        { type: Filter.Design, label: 'Designs' },
-        { type: Filter.Game, label: 'Games' },
+        {type: Filter.Web, label: 'Web apps'},
+        {type: Filter.Design, label: 'Designs'},
+        {type: Filter.Game, label: 'Games'},
     ];
 
-    console.log(ScrollContainer);
     const [filteredProjects, setFilteredProjects] = useState(projects);
     const [activeFilter, setActiveFilter] = useState(Filter.All);
 
@@ -52,7 +51,7 @@ export function ProjectsPage({ projects }: ProjectsPageProps) {
     return (
         <ProjectsPageStyled>
             <Filters>
-                {filters.map(({ label, type }) => (
+                {filters.map(({label, type}) => (
                     <FilterButton
                         accentColor={getAccentColor(type)}
                         mainColor={getMainColor(type)}
@@ -60,9 +59,9 @@ export function ProjectsPage({ projects }: ProjectsPageProps) {
                         key={type}
                         onClick={() => filterProjects(type)}
                     >
-                        {type == Filter.Web && <IconWorld />}
-                        {type == Filter.Design && <IconDragDrop />}
-                        {type == Filter.Game && <IconBrandAppleArcade />}
+                        {type == Filter.Web && <IconWorld/>}
+                        {type == Filter.Design && <IconDragDrop/>}
+                        {type == Filter.Game && <IconBrandAppleArcade/>}
                         {label}
                     </FilterButton>
                 ))}
@@ -70,7 +69,7 @@ export function ProjectsPage({ projects }: ProjectsPageProps) {
             <ProjectCardRow>
                 <ScrollContainer>
                     {filteredProjects.map((project) => (
-                        <ProjectCard key={project.id} project={project} />
+                        <ProjectCard key={project.id} project={project}/>
                     ))}
                 </ScrollContainer>
             </ProjectCardRow>
