3682651399ad3985bf72a4d34caa9ca6cfc826bb
PeanutButte7 <adam.barta007@gmail.com>
2022-11-13 20:47:35 +0100

    Changed head

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,11 @@
-import type { AppProps } from 'next/app';
+import type {AppProps} from 'next/app';
 import '../styles/globals.css';
 
-function MyApp({ Component, pageProps }: AppProps) {
-    return <Component {...pageProps} />;
+function MyApp({Component, pageProps}: AppProps) {
+    return (<>
+        <title>Adam Bárta - webdev & design</title>
+        <Component {...pageProps} />
+    </>);
 }
 
 export default MyApp;
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,23 +1,24 @@
-import NextDocument, { Head, Html, Main, NextScript } from 'next/document';
-import { getCssText } from 'styles';
+import NextDocument, {Head, Html, Main, NextScript} from 'next/document';
+import {getCssText} from 'styles';
 
 export default class Document extends NextDocument {
     render() {
         return (
-            <Html lang='cs'>
+            <Html lang="cs">
                 <Head>
                     <style
-                        id='stitches'
-                        dangerouslySetInnerHTML={{ __html: getCssText() }}
+                        id="stitches"
+                        dangerouslySetInnerHTML={{__html: getCssText()}}
                     />
                     <link
-                        href='https://fonts.googleapis.com/css2?family=Roboto+Flex:opsz,wdth,wght@8..144,100,400;8..144,151,1000&display=swap'
-                        rel='stylesheet'
+                        href="https://fonts.googleapis.com/css2?family=Roboto+Flex:opsz,wdth,wght@8..144,100,400;8..144,151,1000&display=swap"
+                        rel="stylesheet"
+                        type="text/css"
                     />
                 </Head>
                 <body>
-                    <Main />
-                    <NextScript />
+                <Main/>
+                <NextScript/>
                 </body>
             </Html>
         );
