c28c8373a954e5f60d77e7fd5d8f90a89276e2d6
PeanutButte7 <adam.barta007@gmail.com>
2022-12-17 13:45:38 +0100

    :sparkles: Added a proper mobile navbar

--- src/components/Navbar/Navbar.styles.tsx
+++ src/components/Navbar/Navbar.styles.tsx
@@ -1,7 +1,11 @@
-import {styled} from 'styles';
+import { styled } from 'styles';
+import { slideInLeft } from '../../styles/animations';
 
-export const NavbarStyled = styled('div', {
-    display: 'flex',
+export const DesktopNavbarStyled = styled('div', {
+    '@desktop': {
+        display: 'flex',
+    },
+    display: 'none',
     alignItems: 'center',
     flexDirection: 'row',
     justifyContent: 'space-between',
@@ -12,78 +16,106 @@ export const NavbarStyled = styled('div', {
     borderBottom: '2px solid $almostGrey',
     borderTop: 'none',
 
-    '& p': {
+    p: {
         cursor: 'pointer',
 
         '&:hover': {
             textDecoration: 'underline',
         },
     },
-
 });
 
 export const RightSection = styled('div', {
     display: 'flex',
     flexDirection: 'row',
-    gap: 10,
-
-    '@desktop': {
-        gap: 20,
-    },
+    gap: 20,
 });
 
-export const LeftSection = styled('div', {
+export const ImageSection = styled('div', {
     display: 'flex',
+    flexDirection: 'column',
     alignItems: 'center',
-    gap: '1rem',
+    gap: '0.5rem',
+
+    '@desktop': {
+        flexDirection: 'row',
+        gap: '1rem',
+    },
 
     '&:hover': {
         cursor: 'pointer',
 
-        '& img': {
+        img: {
             border: '3px solid $almostGrey',
         },
 
-        '& p': {
+        p: {
             textDecoration: 'underline',
-        }
-    },
-
-    '& img': {
-        display: 'none',
-    },
-
-    '@desktop': {
-        '& img': {
-            display: 'block',
-            width: 48,
-            height: 48,
-            borderRadius: '50%',
-            border: '2px solid $almostGrey',
         },
-    }
+    },
+
+    img: {
+        display: 'block',
+        width: 48,
+        height: 48,
+        borderRadius: '50%',
+        border: '2px solid $almostGrey',
+    },
 });
 
-export const NavigationText = styled('span', {
-    '& svg': {
-        width: 30,
-        height: 30,
-    },
-
-    // Makes the icon hide on desktop and text hide on mobile
-    '& > *:first-child': {
+export const MobileNavbarStyled = styled('div', {
+    '@desktop': {
         display: 'none',
     },
-    '& > *:last-child': {
-        display: 'inline-block',
-    },
 
-    '@desktop': {
-        '& > *:first-child': {
-            display: 'inline-block',
-        },
-        '& > *:last-child': {
-            display: 'none',
+    display: 'flex',
+    alignItems: 'center',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    px: '10vw',
+    py: '1rem',
+
+    backgroundColor: '$almostWhite',
+    borderBottom: '2px solid $almostGrey',
+    borderTop: 'none',
+});
+
+export const MobileMenuOverlay = styled('div', {
+    position: 'fixed',
+    top: 0,
+    left: 0,
+    width: '100vw',
+    height: '100vh',
+    backgroundColor: '$almostWhite',
+    zIndex: 1000,
+    animation: `${slideInLeft} 0.2s ease-out`,
+
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    paddingTop: '15vh',
+    gap: '3rem',
+});
+
+export const MobileLinks = styled('div', {
+    display: 'flex',
+    flexDirection: 'column',
+    gap: '0.5rem',
+    textAlign: 'center',
+
+    p: {
+        cursor: 'pointer',
+        fontSize: '2.5rem',
+
+        '&:hover': {
+            textDecoration: 'underline',
         },
     },
+});
+
+export const MobileMenuButton = styled('button', {
+    cursor: 'pointer',
+    zIndex: 1001,
+    backgroundColor: 'transparent',
+    border: 'none',
 });
--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,50 +1,125 @@
-import {Typography} from 'components/UI';
-import {LeftSection, NavbarStyled, NavigationText, RightSection} from './Navbar.styles';
+import { Typography } from 'components/UI';
+import {
+    DesktopNavbarStyled,
+    ImageSection,
+    MobileLinks,
+    MobileMenuButton,
+    MobileMenuOverlay,
+    MobileNavbarStyled,
+    RightSection,
+} from './Navbar.styles';
 import Link from 'next/link';
-import {IconBook2, IconBrush, IconPhone} from '@tabler/icons';
+import { IconMenu, IconX } from '@tabler/icons';
+import { useState } from 'react';
+import { useRouter } from 'next/router';
 
 export function Navbar() {
+    const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
+    const router = useRouter();
+
+    function closeMenu(newRoute: string) {
+        // check if the current route is the same as the one we are navigating to
+        // if so, close the mobile menu
+        console.log(router.pathname, newRoute);
+        if (router.pathname === newRoute) {
+            setIsMobileMenuOpen(false);
+        }
+    }
+
     return (
         <>
-            <NavbarStyled>
-                <Link href="/" passHref>
-                    <LeftSection>
+            <DesktopNavbarStyled>
+                <Link href='/' passHref>
+                    <ImageSection>
                         <picture>
-                            <source srcSet="/img/headshot.jpg" type="image/jpg"/>
-                            <img src="/img/headshot.jpg" alt="Adam Bárta"/>
+                            <source
+                                srcSet='/img/headshot.jpg'
+                                type='image/jpg'
+                            />
+                            <img src='/img/headshot.jpg' alt='Adam Bárta' />
                         </picture>
-                        <Typography type="h3" size="xxl">
+                        <Typography type='h3' size='xxl'>
                             Adam Bárta
                         </Typography>
-                    </LeftSection>
+                    </ImageSection>
                 </Link>
                 <RightSection>
-                    <Link href="projects" passHref>
-                        <NavigationText>
-                            <Typography type="h3" size="xxl">
-                                Projects
-                            </Typography>
-                            <IconBrush/>
-                        </NavigationText>
+                    <Link href='projects' passHref>
+                        <Typography type='h3' size='xxl'>
+                            Projects
+                        </Typography>
                     </Link>
-                    <Link href="about" passHref>
-                        <NavigationText>
-                            <Typography type="h3" size="xxl">
-                                About me
-                            </Typography>
-                            <IconBook2/>
-                        </NavigationText>
+                    <Link href='about' passHref>
+                        <Typography type='h3' size='xxl'>
+                            About me
+                        </Typography>
                     </Link>
-                    <Link href="contact" passHref>
-                        <NavigationText>
-                            <Typography type="h3" size="xxl">
-                                Contact
-                            </Typography>
-                            <IconPhone/>
-                        </NavigationText>
+                    <Link href='contact' passHref>
+                        <Typography type='h3' size='xxl'>
+                            Contact
+                        </Typography>
                     </Link>
                 </RightSection>
-            </NavbarStyled>
+            </DesktopNavbarStyled>
+            <MobileNavbarStyled>
+                <Link href='/' passHref>
+                    <Typography type='h3' size='xl'>
+                        Adam Bárta
+                    </Typography>
+                </Link>
+                <MobileMenuButton
+                    onClick={() => setIsMobileMenuOpen(!isMobileMenuOpen)}
+                >
+                    {isMobileMenuOpen ? <IconX /> : <IconMenu />}
+                </MobileMenuButton>
+                {isMobileMenuOpen && (
+                    <MobileMenuOverlay>
+                        <Link href='/' passHref>
+                            <ImageSection>
+                                <picture>
+                                    <source
+                                        srcSet='/img/headshot.jpg'
+                                        type='image/jpg'
+                                    />
+                                    <img
+                                        src='/img/headshot.jpg'
+                                        alt='Adam Bárta'
+                                    />
+                                </picture>
+                                <Typography type='h3' size='xxl'>
+                                    Adam Bárta
+                                </Typography>
+                            </ImageSection>
+                        </Link>
+                        <MobileLinks>
+                            <Link href='projects' passHref>
+                                <Typography
+                                    type='h3'
+                                    onClick={() => closeMenu('/projects')}
+                                >
+                                    Projects
+                                </Typography>
+                            </Link>
+                            <Link href='about' passHref>
+                                <Typography
+                                    type='h3'
+                                    onClick={() => closeMenu('/about')}
+                                >
+                                    About me
+                                </Typography>
+                            </Link>
+                            <Link href='contact' passHref>
+                                <Typography
+                                    type='h3'
+                                    onClick={() => closeMenu('/contact')}
+                                >
+                                    Contact
+                                </Typography>
+                            </Link>
+                        </MobileLinks>
+                    </MobileMenuOverlay>
+                )}
+            </MobileNavbarStyled>
         </>
     );
 }
